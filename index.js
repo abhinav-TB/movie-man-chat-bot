e9621ed48cfaf0fc4a72e620c6757149064eb1ca
abhinav-TB <abhinavtb@gmail.com>
2020-03-01 18:00:44 +0530

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+require('dotenv').config();
 // replace the value below with the Telegram token you receive from @BotFather
-const token = `1139073305:AAGdLCGJIFIHOMoMhiC51-PGL0c3-5mGneA`;
+
+const token = process.env.TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
